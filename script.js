e0f029fcaa14d88293ae77db2c5df73abedce1aa
Djo-75 <j.pecquet@live.fr>
2023-03-06 14:48:40 +0100

    exo 1 done

---
+++ script.js
@@ -0,0 +1,21 @@
+// Feature 1
+
+var feature1 = document.querySelector("footer")
+    console.log(feature1)
+
+var clique = function() {
+    console.log("clique");}
+
+feature1.addEventListener("click", clique)
+
+// Feature 1 bis
+
+
+
+var feature1 = document.querySelector("footer")
+    console.log(feature1)
+    
+var clique = function() {
+    console.log("clique");}
+
+feature1.addEventListener("click", clique)
