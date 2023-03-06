0b2b49822a88e36012b0144599e9b5142c52eb88
Djo-75 <j.pecquet@live.fr>
2023-03-06 15:05:32 +0100

    1 bis done

--- script.js
+++ script.js
@@ -10,12 +10,15 @@ feature1.addEventListener("click", clique)
 
 // Feature 1 bis
 
-
+let counter = 0
 
 var feature1 = document.querySelector("footer")
     console.log(feature1)
-    
+
 var clique = function() {
-    console.log("clique");}
+    console.log(`clic numéro ${++counter}`)
+}
 
 feature1.addEventListener("click", clique)
+
+// Feature 2
